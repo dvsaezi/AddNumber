d6b09b10e039710e1ebaf9198e255109449e1209
dvsaezi <59031184+dvsaezi@users.noreply.github.com>
2021-04-30 10:45:57 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+namespace AddNumber
+{
+    static class Program
+    {
+        static int AddNumber(int x)
+        {
+            return x + 1;
+        }
+        static async Task Main() => await OwO();
+
+        private static Task OwO()
+        {
+            Console.WriteLine("Enter the number you want to know the next value that comes after : ");
+            var input = int.Parse(Console.ReadLine());
+            var res = AddNumber(input);
+            Console.WriteLine($"The next number that comes after {input} is {res}");
+            Console.ReadLine();
+            return Task.CompletedTask;
+        }
+    }
+}
